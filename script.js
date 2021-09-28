e08a9d43ae2c8c0efb412ba6083d90a3d17a3915
luciamorelli <luciamorelli00@gmail.com>
2021-09-28 01:30:05 -0300

    Modificaciones estructura de pagina

---
+++ script.js
@@ -0,0 +1,33 @@
+//Librreria Scroll Reveal
+window.sr = ScrollReveal();
+
+    sr.reveal('.imagen1', {
+        duration: 4000,
+        origin: "bottom",
+        distance: '-200px'
+    });
+
+    
+    sr.reveal('.imagen2', {
+        duration: 4000,
+        origin: "bottom",
+        distance: '-200px'
+    });
+
+    sr.reveal('.imagen3', {
+        duration:4000,
+        origin: "bottom",
+        distance: '-200px'
+    });
+
+    sr.reveal('.title', {
+        duration:4000,
+        origin: "right",
+        distance: '-200px'
+    });
+    
+    sr.reveal('.main__section2--contenedor', {
+        duration:4000,
+        origin: "right",
+        distance: '-400px'
+    });
